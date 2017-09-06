9e4893c4548f6acacab93617a3754b9102c4ec4a
JuhoP <juzeliuz@suomi24.fi>
2017-09-06 13:43:57 +0900

    Reorganized script folders. Started working on EnemySpawner and EnemyBase classes. Renamed CleanerBot to Poss_CleanerBot. Removed obsolete scripts.

---
+++ TDSBSG/Assets/Scripts/Controllers/CameraController.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    Toolbox toolbox;
+    EventManager em;
+
+    public Transform target;
+    public float xOffset = 0;
+    public float yOffset = 0;
+    public float zOffset = 0;
+    float smoothTime = 0.25f;
+    Vector3 velocity = Vector3.zero;
+    bool isFollowing = false;
+
+    private void Awake()
+    {
+        toolbox = FindObjectOfType<Toolbox>();
+        em = toolbox.GetComponent<EventManager>();
+
+    }
+
+    private void OnEnable()
+    {
+        em.OnGameStarted += OnGameStarted;
+    }
+
+    private void OnDisable()
+    {
+        em.OnGameStarted -= OnGameStarted;
+    }
+
+    private void LateUpdate()
+    {
+
+        if (isFollowing)
+        {
+            //Follow target
+            Vector3 targetPosition = target.position;
+            Vector3 desiredPosition = new Vector3(targetPosition.x + xOffset,
+                targetPosition.y + yOffset, targetPosition.z + zOffset);
+            //transform.position = desiredPosition;
+
+            transform.position = Vector3.SmoothDamp(transform.position,
+                desiredPosition, ref velocity, smoothTime);
+
+            //transform.LookAt(target);
+            //transform.eulerAngles = new Vector3(transform.eulerAngles.x, 0, 0);
+        }
+    }
+
+    void OnGameStarted()
+    {
+        isFollowing = true;
+        Vector3 targetPosition = target.position;
+        Vector3 desiredPosition = new Vector3(targetPosition.x + xOffset,
+            targetPosition.y + yOffset, targetPosition.z + zOffset);
+        transform.position = desiredPosition;
+    }
+
+}
---
+++ TDSBSG/Assets/Scripts/Controllers/EnemyBase.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBase : MonoBehaviour
+{
+    bool isAlerted = false;
+    bool isTrackingTarget = false;
+    readonly bool canPatrol = true;
+    List<Transform> patrolPoints = new List<Transform>();
+
+    public bool GetIsAlerted()
+    {
+        return isAlerted;
+    }
+
+    public bool GetIsTrackingTarget()
+    {
+        return isTrackingTarget;
+    }
+
+    public void SetPatrolPoints(List<Transform> newPatrolPoints)
+    {
+
+    }
+
+    //TODO: Implement method for detecting targets (raycast?)
+
+}
