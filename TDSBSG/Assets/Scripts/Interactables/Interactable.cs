30efce5e329aa0cb5404c67f18cc180af1c881a6
Syunpei <grean1325@icloud.com>
2017-09-07 17:03:33 +0900

    create new folder of script,create Interactab class, create Interactab_Liftable class

---
+++ TDSBSG/Assets/Scripts/Interactables/Interactable.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactable : MonoBehaviour {
+    [SerializeField]
+    private bool isInUse;
+
+    public bool GetIsInUse() { return isInUse; }
+
+    public virtual bool StartInteractable()
+    {
+        if (isInUse) { return false; }
+
+        isInUse = true;
+        return true;
+    }
+
+    public virtual void EndInteractable()
+    {
+        isInUse = false;
+    }
+}
---
+++ TDSBSG/Assets/Scripts/Interactables/Interactable_Liftable.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactable_Liftable : Interactable {
+
+    float moveTime = 1.0f;     // Time taken to move
+    float elapsedTime = 0.0f;  // elapsed time
+
+    Vector3 startPosition;    // Initial position
+    Vector3 endPosition;      // Position after movement
+
+    // Use this for initialization
+    void Start () {
+        startPosition = transform.position;
+        endPosition = transform.position;
+        endPosition.y += 1.0f;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.Z))
+        {
+            StartInteractable();
+        }
+        if (Input.GetKey(KeyCode.X))
+        {
+            EndInteractable();
+        }
+
+        // Use Lerp function for moving
+        if (GetIsInUse())
+        {
+            // Clamp elapsedTime on maximum time
+            if (elapsedTime >= moveTime) { elapsedTime = moveTime; }
+
+            elapsedTime += moveTime * Time.deltaTime;
+            transform.position = Vector3.Lerp(startPosition, endPosition, elapsedTime);
+        }
+        else
+        {
+            // Clamp elapsedTime on minimum time
+            if (elapsedTime <= 0) { elapsedTime = 0; }
+
+            elapsedTime -= moveTime * Time.deltaTime;
+            transform.position = Vector3.Lerp(startPosition, endPosition, elapsedTime);
+        }
+	}
+
+    public override bool StartInteractable()
+    {
+        if (!base.StartInteractable()) { return false; }
+
+        //transform.Translate(0, 1.0f, 0);
+        return false;
+    }
+
+    public override void EndInteractable()
+    {
+        base.EndInteractable();
+    }
+}
