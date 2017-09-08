d822ae96f32d455f431ebdf0c626996911abaf77
shotake8 <shotake888@gmail.com>
2017-09-08 14:25:41 +0900

    Create ApplicaionManager script

---
+++ TDSBSG/Assets/Scripts/Managers/ApplicationManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ApplicationManager : MonoBehaviour {
+
+	#region References & variables
+	public static ApplicationManager _instance;
+
+	Toolbox toolbox;
+	EventManager em;
+
+	string currentSceneName = null;
+	#endregion
+
+	private void Awake() {
+		if (_instance == null) {
+			_instance = this;
+		}
+		else if (_instance != this) {
+			Destroy(gameObject);
+			return;
+		}
+		DontDestroyOnLoad(gameObject);
+
+		toolbox = FindObjectOfType<Toolbox>();
+		em = toolbox.GetComponent<EventManager>();
+	}
+
+	void OnLevelFinishedLoading(scene)
+}
