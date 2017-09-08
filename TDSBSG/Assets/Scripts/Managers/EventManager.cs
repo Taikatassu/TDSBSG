6d1e08c499e4b42b60630e643110f4cd0eb380fe
shotake8 <shotake888@gmail.com>
2017-09-08 14:33:02 +0900

    Add new event what finish load scene

--- TDSBSG/Assets/Scripts/Managers/EventManager.cs
+++ TDSBSG/Assets/Scripts/Managers/EventManager.cs
@@ -2,104 +2,91 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EventManager : MonoBehaviour
-{
-    public static EventManager _instance;
+public class EventManager : MonoBehaviour {
+	public static EventManager _instance;
 
-    private void Awake()
-    {
-        if (_instance == null)
-        {
-            _instance = this;
-        }
-        else if (_instance != this)
-        {
-            Destroy(gameObject);
-            return;
-        }
+	private void Awake() {
+		if (_instance == null) {
+			_instance = this;
+		}
+		else if (_instance != this) {
+			Destroy(gameObject);
+			return;
+		}
 
-        DontDestroyOnLoad(gameObject);
-    }
+		DontDestroyOnLoad(gameObject);
+	}
 
-    public delegate void EmptyVoid();
-    public delegate void InputVoid(EInputType inputType);
-    public delegate void IntBoolVoid(int integer, bool boolean);
-    public delegate void GameObjectVoid(GameObject go);
-    public delegate GameObject EmptyGameObject();
+	public delegate void EmptyVoid();
+	public delegate void StringVoid(string str);
+	public delegate void InputVoid(EInputType inputType);
+	public delegate void IntBoolVoid(int integer, bool boolean);
+	public delegate void GameObjectVoid(GameObject go);
+	public delegate GameObject EmptyGameObject();
 
-    public event EmptyVoid OnInitializeGame;
-    public void BroadcastInitializeGame()
-    {
-        if (OnInitializeGame != null)
-        {
-            OnInitializeGame();
-        }
-    }
+	public event EmptyVoid OnInitializeGame;
+	public void BroadcastInitializeGame() {
+		if (OnInitializeGame != null) {
+			OnInitializeGame();
+		}
+	}
 
-    public event EmptyVoid OnStartGame;
-    public void BroadcastStartGame()
-    {
-        if (OnStartGame != null)
-        {
-            OnStartGame();
-        }
-    }
+	public event EmptyVoid OnStartGame;
+	public void BroadcastStartGame() {
+		if (OnStartGame != null) {
+			OnStartGame();
+		}
+	}
 
-    public event InputVoid OnInputEvent;
-    public void BroadcastInputEvent(EInputType newInput)
-    {
-        if (OnInputEvent != null)
-        {
-            OnInputEvent(newInput);
-        }
-    }
+	public event InputVoid OnInputEvent;
+	public void BroadcastInputEvent(EInputType newInput) {
+		if (OnInputEvent != null) {
+			OnInputEvent(newInput);
+		}
+	}
 
-    public event EmptyGameObject OnRequestPlayerReference;
-    public GameObject BroadcastRequestPlayerReference()
-    {
-        if (OnRequestPlayerReference != null)
-        {
-            return OnRequestPlayerReference();
-        }
-        else
-        {
-            return FindObjectOfType<Player>().gameObject;
-        }
-    }
+	public event EmptyGameObject OnRequestPlayerReference;
+	public GameObject BroadcastRequestPlayerReference() {
+		if (OnRequestPlayerReference != null) {
+			return OnRequestPlayerReference();
+		}
+		else {
+			return FindObjectOfType<Player>().gameObject;
+		}
+	}
 
-    public event GameObjectVoid OnRegisterEnemy;
-    public void BroadcastRegisterEnemy(GameObject newEnemy)
-    {
-        if (OnRegisterEnemy != null)
-        {
-            OnRegisterEnemy(newEnemy);
-        }
-    }
+	public event GameObjectVoid OnRegisterEnemy;
+	public void BroadcastRegisterEnemy(GameObject newEnemy) {
+		if (OnRegisterEnemy != null) {
+			OnRegisterEnemy(newEnemy);
+		}
+	}
 
-    public event IntBoolVoid OnRoomEntered;
-    public void BroadcastRoomEntered(int roomSecurityLevel, bool isAllowed)
-    {
-        if (OnRoomEntered != null)
-        {
-            OnRoomEntered(roomSecurityLevel, isAllowed);
-        }
-    }
+	public event IntBoolVoid OnRoomEntered;
+	public void BroadcastRoomEntered(int roomSecurityLevel, bool isAllowed) {
+		if (OnRoomEntered != null) {
+			OnRoomEntered(roomSecurityLevel, isAllowed);
+		}
+	}
 
-    public event IntBoolVoid OnDoorEntered;
-    public void BroadcastDoorEntered(int doorSecurityLevel, bool isAllowed)
-    {
-        if (OnDoorEntered != null)
-        {
-            OnDoorEntered(doorSecurityLevel, isAllowed);
-        }
-    }
+	public event IntBoolVoid OnDoorEntered;
+	public void BroadcastDoorEntered(int doorSecurityLevel, bool isAllowed) {
+		if (OnDoorEntered != null) {
+			OnDoorEntered(doorSecurityLevel, isAllowed);
+		}
+	}
 
-    public event EmptyVoid OnStartAlarm;
-    public void BroadcastStartAlarm()
-    {
-        if (OnStartAlarm != null)
-        {
-            OnStartAlarm();
-        }
-    }
+	public event EmptyVoid OnStartAlarm;
+	public void BroadcastStartAlarm() {
+		if (OnStartAlarm != null) {
+			OnStartAlarm();
+		}
+	}
+
+	public event StringVoid OnFinisheLoading;
+	public void BroadcastRequestLoadLevel(string nameOfLoadScene) {
+		if (OnFinisheLoading != null) {
+			OnFinisheLoading(nameOfLoadScene);
+		}
+	}
 }
