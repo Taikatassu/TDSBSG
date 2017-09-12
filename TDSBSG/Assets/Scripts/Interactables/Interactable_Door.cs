797ea0b1fb634f0ff7f370f4213afad965f3600c
Shota Takahashi <shotake888@gmail.com>
2017-09-12 13:40:36 +0900

    Implemented Interactable_door.

--- TDSBSG/Assets/Scripts/Interactables/Interactable_Door.cs
+++ TDSBSG/Assets/Scripts/Interactables/Interactable_Door.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Intractable_Door : Interactable {
+public class Interactable_Door : Interactable {
 
     [SerializeField]
     Collider interactionTrigger;
@@ -18,12 +18,29 @@ public class Intractable_Door : Interactable {
     [SerializeField]
     float animationDuration = 1.0f;
 
+    EDoorState state;
+
+    private enum EDoorState {
+        IS_WAITING,
+        IS_OPENING,
+        IS_CLOSING
+    }
+
     private void Start() {
         permissionList = new List<ERobotType>();
+        permissionList.Add(ERobotType.DEFAULT);
         permissionList.Add(ERobotType.SMALL);
 
         closedPos = doorObject.transform.position;
         closedRot = doorObject.transform.eulerAngles;
+    }
+
+    private void Update() {
+        if (state == EDoorState.IS_OPENING) {
+            Open();
+        } else if (state == EDoorState.IS_CLOSING) {
+            Close();
+        }
     }
 
     protected override float InteractionStartDuration() {
@@ -40,7 +57,6 @@ public class Intractable_Door : Interactable {
         if (user.GetGameObject().GetComponent<Poss_Mobile>()) {
             Poss_Mobile userMobile = user.GetGameObject().GetComponent<Poss_Mobile>();
             Vector3 userPostion = userMobile.transform.position;
-            //userMobile.SetDestination()
         }
         return startDurationTime;
     }
@@ -53,9 +69,50 @@ public class Intractable_Door : Interactable {
 
     private void Open() {
 
+        startDurationTime += Time.deltaTime;
+        if(startDurationTime >= animationDuration) {
+            startDurationTime = animationDuration;
+        }
+        endDurationTime = animationDuration - startDurationTime;
+        doorObject.transform.position = Vector3.Lerp(closedPos, openPos, startDurationTime);
+        doorObject.transform.eulerAngles = Vector3.Lerp(closedRot, openRot, startDurationTime);
     }
 
     private void Close() {
+        endDurationTime += Time.deltaTime;
+        if (endDurationTime >= animationDuration) {
+            endDurationTime = animationDuration;
+        }
+        startDurationTime = animationDuration - endDurationTime;
+        doorObject.transform.position = Vector3.Lerp(openPos, closedPos, endDurationTime);
+        doorObject.transform.eulerAngles = Vector3.Lerp(openRot, closedRot, endDurationTime);
+    }
 
+    private void OnTriggerEnter(Collider other) {
+        Debug.Log("enter door");
+        if (!other.GetComponent(typeof(IPossessable))) {
+            return;
+        }
+        IPossessable user = other.GetComponent<IPossessable>();
+        if(!user.GetIsPossessed()) {
+            return;
+        }
+        if (ContainPermissionList(user.GetRobotType())) {
+            state = EDoorState.IS_OPENING;
+        }
+    }
+
+    private void OnTriggerExit(Collider other) {
+        Debug.Log("exit door");
+        if (!other.GetComponent(typeof(IPossessable))) {
+            return;
+        }
+        IPossessable user = other.GetComponent<IPossessable>();
+        if (!user.GetIsPossessed()) {
+            return;
+        }
+        if (ContainPermissionList(user.GetRobotType())) {
+            state = EDoorState.IS_CLOSING;
+        }
     }
 }
